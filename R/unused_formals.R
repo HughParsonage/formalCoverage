79f808c471aa74e1704588e15ae90a7fad6ca66f
HughParsonage <hugh.parsonage@gmail.com>
2017-07-16 15:29:45 +1000

    Add namespace option to unused_formals

--- R/unused_formals.R
+++ R/unused_formals.R
@@ -1,5 +1,6 @@
 #' Is every function argument used in its body?
 #' @param fn A function.
+#' @param ns If \code{fn} is a character vector, the namespace to which \code{fn} belongs.
 #' @return The arguments of \code{fn} that are not used or if all are indeed used \code{NULL} invisibly.
 #' 
 #' @examples 
@@ -8,10 +9,18 @@
 #' }
 #' 
 #' unused_formals(add3)
-#' 
+#' @importFrom utils getFromNamespace
 #' @export
 
-unused_formals <- function(fn) {
+unused_formals <- function(fn, ns = NULL) {
+  if (is.character(fn)) {
+    if (is.null(ns)) {
+      stop("ns must be used if fn is a character vector.")
+    } else {
+      fn <- getFromNamespace(fn, ns = ns)
+    }
+  }
+  
   formalz <- formals(fn)
   bod <- as.character(body(fn))
   
